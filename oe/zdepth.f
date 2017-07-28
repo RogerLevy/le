dba0a476fa651d9c5b881e38390a4cfae40ae10e
rogerlevy <roger.levy@gmail.com>
2017-07-28 19:42:23 -0400

    Flesh out zdepth extension

--- oe/zdepth.f
+++ oe/zdepth.f
@@ -3,3 +3,16 @@
 obj:
     var zdepth
     ext
+
+: compiled  ( addr -- addr cells )  here over - cell/ s>p ;
+
+\ Depthsort by Y position
+: zdepth@  's zdepth @ ;
+: sort  ['] zdepth@ rsort ;
+
+\ Not sure if I need to factor any of this or just copy and paste.
+: drawem  ( addr cells -- )  cells bounds do  i @ me!  draw  cell +loop ;
+: enqueue  eachlist>  hide @ ?exit  me , ;
+: draw-sorted  here dup  enqueue  compiled  2dup sort  drawem  reclaim ;
+
+' draw-sorted is render
