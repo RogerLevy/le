5efc2560f53a93f92600e89766747c38b1398185
rogerlevy <roger.levy@gmail.com>
2017-08-09 13:17:34 -0400

    Add mo/loadtmx.f

---
+++ mo/loadtmx.f
@@ -0,0 +1,83 @@
+\ Load and display tilemap and objects (no interaction, just scroll around with arrow keys)
+le:
+    import bu/mo/tmx
+idiom loadtmx:
+    import bu/mo/tilegame
+    import bu/mo/array2d
+    import bu/mo/xml
+
+
+16384 cellstack bitmaps
+0 value ts
+create tempimg  /image /allot
+16384 cellstack objtypes
+2048 2048 array2d tilemap
+
+var gid
+augment
+
+
+\ defers.  they assume ONE has already been called.
+\ these can assume the GID has already been set; same for HIDE.
+defer onmapload  ' noop is onmapload  ( -- )  \ execute BEFORE the initializer
+defer obj  ' noop  is obj   ( -- )  \ default initializer when type isn't specified
+defer box  ' 2drop is box  ( w h -- )
+
+\ make array of spawner XT's.  if the "type" is defined in the tile, it's
+\ looked up in the dictionary and if it exists we get the XT and put it in the array.
+\ if it doesn't exist or if the "type" is not defined we use ' *OBJ
+\ no need to truncate the objtypes stack since we'll be using GID's as indices.
+\ if a tile element doesn't exist at all we of course use ' *OBJ then too.
+: make-initializers  ( -- )
+    ts @tilecount for  ['] obj  ts @firstgid i +  objtypes [] !  loop  \ default all to OBJ
+    ts tiles>  ( tile-node )  >r                                        \ process any tile nodes
+        r@ ?type if  uncount find not if  drop  ['] obj  then
+               else  ['] obj  then
+        ts @firstgid  r@ @id +  objtypes [] !
+    r> drop
+;
+
+
+: load-tiles
+    bitmaps scount for  @+ -bmp  loop drop  bitmaps 0 truncate
+    clear-tiles
+    #tilesets for
+        i tileset[] to ts
+        ts multi-image? if    \ might not work with "weird" tilesets ... if anyone even does that kind of thing
+            \ add tiles that use their own image files
+            ts @tilecount for
+                tempimg ts i tile-image load-image
+                tempimg bmp @ ts i tile-gid tiles [] !
+                tempimg bmp @ bitmaps push
+            loop
+        else
+            \ add tiles from single image
+            tempimg ts single-image load-image
+            tempimg ts tile-dims ts @firstgid change-tiles
+            tempimg bmp @ bitmaps push
+        then
+        make-initializers
+    loop ;
+
+: load-objects  ( objgroup-node -- )
+    \ get the destination objlist first
+    @name uncount find if  >body  else  objects  then  in
+    objects> >r
+        r@ x.
+        r@ @xy at
+        r@ ?name if
+            evaluate
+        else
+            r@ rectangle? not if
+                ONE  r@ @gid gid !
+                r@ @visible not hide !
+                ONMAPLOAD
+                gid @ objtypes [] @ execute
+            else
+                r@ @wh ONE BOX
+            then
+        then
+    r> drop ;
+
+\ : load-all-objects ;
+
