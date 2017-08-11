578714ba74a41b123b020fcc70ce1ddd2b9576eb
rogerlevy <roger.levy@gmail.com>
2017-08-11 03:18:09 -0400

    ONMAPLOAd now executes the initializer; strings now evaluated not looked-up

--- mo/loadtmx.f
+++ mo/loadtmx.f
@@ -10,29 +10,30 @@ idiom loadtmx:
 16384 cellstack bitmaps
 0 value ts
 create tempimg  /image /allot
-16384 cellstack objtypes
+16384 cellstack initializers
 
 var gid
 augment
 
 \ defers.  they assume ONE has already been called.
 \ these can assume the GID has already been set; same for HIDE.
-defer onmapload  ' noop is onmapload  ( -- )  \ execute BEFORE the initializer
+defer onmapload  ' execute is onmapload  ( initializer -- )  \ executes the initializer (or not)
 defer obj  ' noop  is obj   ( -- )  \ default initializer when type isn't specified
 defer box  ' 2drop is box  ( w h -- )
 
 \ make array of spawner XT's.  if the "type" is defined in the tile, it's
 \ looked up in the dictionary and if it exists we get the XT and put it in the array.
 \ if it doesn't exist or if the "type" is not defined we use ' *OBJ
-\ no need to truncate the objtypes stack since we'll be using GID's as indices.
+\ no need to truncate the initializers stack since we'll be using GID's as indices.
 \ if a tile element doesn't exist at all we of course use ' *OBJ then too.
-: (defaults)  ts @tilecount for  ['] obj  ts @firstgid i +  objtypes [] !  loop ; \ default all to OBJ
+: (defaults)  ts @tilecount for  ['] obj  ts @firstgid i +  initializers [] !  loop ; \ default all to OBJ
 : make-initializers  ( -- )
     (defaults)
     ts tiles>  ( tile-node )  >r                                        \ process any tile nodes
-        r@ ?type if  uncount find not if  drop  ['] obj  then
+        r@ ?type if  ['] evaluate  catch if  bright ." ERROR evaluating tile type, continuing..." normal
+            2drop ['] obj  then
                else  ['] obj  then
-        ts @firstgid  r@ @id +  objtypes [] !
+        ts @firstgid  r@ @id +  initializers [] !
     r> drop
 ;
 
@@ -60,18 +61,19 @@ defer box  ' 2drop is box  ( w h -- )
 
 : load-objects  ( objgroup-node -- )
     \ get the destination objlist first
-    dup @name uncount find if  >body  else  drop  objects  then  in
+    dup @name ['] evaluate catch if
+        2drop objects  bright ." ERROR evaluating object group name (objlist), continuing..." normal
+    then  in
     objects> >r
         cr r@ x.
         r@ @xy at
         r@ ?name if
-            evaluate
+            ['] evaluate catch if  bright ." ERROR evaluating object name, continuing..." normal then
         else
             r@ rectangle? not if
                 ONE  r@ @gid gid !
                 r@ @visible not hide !
-                ONMAPLOAD
-                gid @ objtypes [] @ execute
+                gid @ initializers [] @ ONMAPLOAD
             else
                 r@ @wh ONE BOX
             then
